01bc06ed5c3bca61a0a034feb398268a31a26455
Matt Pedersen <mpedersen2054@gmail.com>
2015-08-17 14:26:09 -0500

    add bootstrap-selectbox and code to make the artwork-thumbs on the index page to be sorted alphabetically

--- public/js/app.js
+++ public/js/app.js
@@ -13,11 +13,57 @@ function searchFormSubmit() {
   searchForm.on('submit', function(e) {
     e.preventDefault();
     window.location.href = 'http://localhost:3000/search?term=' + searchInput.val();
-  })
+  });
+}
+
+function selectPickerSort() {
+  var sp = $('.selectpicker');
+  sp.on('change', function(e) {
+    e.preventDefault();
+    var spVal = sp.val();
+    chopAndOrder(spVal);
+  });
+}
+
+// chop and order artwork-thumbs accordingly
+function chopAndOrder(o) {
+  var order = o || '';
+  var thumbs = $('.artwork-thumb');
+  var artworksContainer = $('.artworks-container');
+
+  thumbs.detach();
+
+  if (order === 'alphabetical') {
+    // good sort example
+    thumbs.sort(function(a, b) {
+      a = $(a).data('peicename');
+      b = $(b).data('peicename');
+      if (a > b) { return 1; }
+      else if (a < b) { return -1; }
+      else { return 0 }
+    });
+
+    thumbs.each(function() { artworksContainer.append(this); });
+
+  }
+
+  if (order === 'artist') {
+    console.log('artist!!', tNames)
+  }
+
+  if (order === 'type') {
+    console.log('type!!', tNames)
+  }
+
+  if (order === 'date') {
+    console.log('date!!', tNames)
+  }
+
 }
 
 $(document).ready(function() {
+  $('.selectpicker').selectpicker();
   navbarAffix();
   searchFormSubmit();
-  $('img').unveil(300);
+  selectPickerSort();
 });
