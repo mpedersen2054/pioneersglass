54bdcb63f7e193d6781827c25fd6b076389786b7
Matt Pedersen <mpedersen2054@gmail.com>
2015-08-17 19:04:38 -0500

    and again

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ db.once('open', function() { console.log('~~ connected to mongodb://'+dbName+' ~
 
 
 // middleware
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views'));
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 app.use(bodyParser.json());
